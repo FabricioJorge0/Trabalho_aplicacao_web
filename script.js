bb0cbf328a4b16dd4ee66dc4344ba53544c8cb4c
Fabricio Jorge <fabricioj.dev@gmail.com>
2023-09-20 00:07:07 -0400

    primeiro commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// todas essas variaveis estão armazenando os ID criados no cabeçalho do site para fazer a animção
+
+var sobreMim = document.getElementById("sobreMim")
+var formacao = document.getElementById("formacao")
+var chegar = document.getElementById("footer")
+
+
+// aqui eu estou carregando a tela(window) e adicionando um evento para acontecer na tela quando houver o scroll da tela
+window.addEventListener("scroll", function(){
+    
+    /*nessas sequências de ifs, estou criando as animação de acordo com o total de scroll que a tela der.
+    até o momento eu não sei fazer o scroll pelos ids de cada local, como o "sobre mim", "formação" etc
+    então, se for usada em uma tela pequena menor que 1360px essa animação pode apresentar falhas e acontecer antes do esperado */
+    
+    if(this.window.scrollY <= 635){
+        sobreMim.style.backgroundColor = "#494848"
+        formacao.style.backgroundColor = "#353535"
+    }
+    else if(this.window.scrollY >= 636){
+        sobreMim.style.backgroundColor = "#353535"
+        formacao.style.backgroundColor = "#494848"
+    }
+})
+
+alert(window.scrollY)
